32b7a4556dfc6447f83a6422f126b5f9ee2f2833
amestr1s <230774380+amestr1s@users.noreply.github.com>
2025-10-10 00:55:09 +0300

    Complete logic, styling needed

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
+//initialize library array
 const myLibrary = [];
 
 
@@ -14,36 +15,74 @@ function Book(id, title, author, pages, read) {
     
 }
 
-// const book1 = new Book('mitsos', 'mitsopoulos', '332', 'yes');
-// const book2 = new Book(crypto.randomUUID() ,'mitsoss', 'mitsopouloss', '3323', 'no');
-// // const book3 = Book('mitsoss', 'mitsopouloss', '3323', 'no'); //throws error as expected
-// // book1.reportBook();
-// console.log(book2.reportBook());
-
+//function to add book objects in myLibrary array
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(crypto.randomUUID(), title, author, pages, read));
 }
 
-// addBookToLibrary("mitsaki", "mitsoulaki", "322", "read");
-// addBookToLibrary("mitsakis", "mitsoulakis", "342", "notread");
-// console.log(myLibrary);
-
-
+//function to show and refresh the library and also to add status buttons and delete buttons
 const mainLib = document.querySelector(".mainLib");
-
 function displayLibrary() {
     while (mainLib.hasChildNodes()) {
-        mainLib.removeChild(mainLib.firstChild);
+        mainLib.removeChild(mainLib.firstChild); //remove children to avoid duplication
     }
     for (const element of myLibrary) {
         const newP = document.createElement("p");
-        const newContent = document.createTextNode(`${element.title} by ${element.author}, ${element.pages} pages long.`);
+        const delBtn = document.createElement("button");
+        delBtn.setAttribute("class", "delBtn");
+        const delButtonContent = document.createTextNode(`Delete Book`);
+        delBtn.appendChild(delButtonContent);
+        const statusBtn = document.createElement("button");
+        statusBtn.setAttribute("class", "statusBtn");
+        const statusButtonContent = document.createTextNode(`Change Status`);
+        statusBtn.appendChild(statusButtonContent);
+        const bookId = element.id;
+        newP.setAttribute("id", bookId);
+        
+        statusBtn.addEventListener("click", (event) => {
+        
+            if (element.read == "Read") {
+                Object.defineProperty(element, "read", {
+                    value: "Not Read"
+                }); 
+                newContent.remove();
+                newContent = document.createTextNode(`${element.title} by ${element.author}, ${element.pages} pages long. Status = ${element.read}`);
+                newP.appendChild(newContent);
+                console.log(myLibrary);
+            } else if (element.read == "Not Read") {
+                Object.defineProperty(element, "read", {
+                    value: "Read"
+                }); 
+                newContent.remove();
+                newContent = document.createTextNode(`${element.title} by ${element.author}, ${element.pages} pages long. Status = ${element.read}`);
+                newP.appendChild(newContent);
+                console.log(myLibrary);
+            }
+        });
+
+        delBtn.addEventListener("click", (event) => {
+            
+            document.getElementById(bookId).remove();
+            delBtn.remove();
+            statusBtn.remove();
+            const index = myLibrary.findIndex(element => element.id === bookId);
+            
+            if (index !== -1) {
+                myLibrary.splice(index, 1);
+            }
+                console.log(myLibrary);
+        });
+        
+        let newContent = document.createTextNode(`${element.title} by ${element.author}, ${element.pages} pages long. Status = ${element.read}`);
         newP.appendChild(newContent);
         mainLib.appendChild(newP);
+        mainLib.appendChild(delBtn);
+        mainLib.appendChild(statusBtn);
+        console.log(myLibrary);
     }
 }
 
-// displayLibrary();
+
 
 const showForm = document.querySelector("#showForm");
 const bookDialog = document.querySelector("#bookDialog");
@@ -60,28 +99,21 @@ showForm.addEventListener("click", () => {
 });
 
 confirmBtn.addEventListener("click", (event) => {
+  
   event.preventDefault(); // We don't want to submit this fake form
   
   const status = document.querySelector('input[name="status"]:checked')?.value;
   
   addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, status);
 
-  bookDialog.close(); // Have to send the select box value here.
-//   document.querySelector("#title").value = "";
-//   document.querySelector("#author").value = "";
-//   document.querySelector("#pages").value = "";
-//   document.querySelector("#read").checked = true;
+  bookDialog.close(); 
   bookForm.reset();
   displayLibrary();
 });
 
 cancelBtn.addEventListener("click", (event) => {
   
-  bookDialog.close(); // Have to send the select box value here.
-//   document.querySelector("#title").value = "";
-//   document.querySelector("#author").value = "";
-//   document.querySelector("#pages").value = "";
-//   document.querySelector("#read").checked = true;
+  bookDialog.close();
   bookForm.reset();
   displayLibrary();
 });
